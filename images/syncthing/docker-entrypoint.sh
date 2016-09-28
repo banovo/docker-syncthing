b67c308a97f66f033266fc346a82138ae0a309d0
Patrick Kaiser <github@kaiser.xyz>
2016-09-28 16:16:36 +0200

    Initial working version of syncthing server image (#3)
    - remove unneeded startup scripts
    - fix common timezone problem (all images)
    - refactor docker-compose environment-vars usage

---
+++ images/syncthing/docker-entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -eo pipefail
+
+CONTAINER_NAME=${CONTAINER_NAME:-syncthing}
+CONTAINER_USER=${CONTAINER_USER:-run}
+LOCAL_UID=${LOCAL_UID:-1000}
+LOCAL_GID=${LOCAL_GID:-1000}
+
+echo "[`date`] Bootstrapping container ${CONTAINER_NAME}..."
+
+# Change uid + gid for container user based on env vars
+# -----------------------------------------------------------------------------
+echo "[`date`] Changing local user ${CONTAINER_USER} to UID: ${LOCAL_UID}, GID: ${LOCAL_GID} ..."
+groupmod -o -g ${LOCAL_GID} ${CONTAINER_USER}
+usermod -o -u ${LOCAL_UID} -g ${LOCAL_GID} ${CONTAINER_USER}
+
+# Fix Perms
+# -----------------------------------------------------------------------------
+chown -R "${LOCAL_UID}":"${LOCAL_GID}" /home/${CONTAINER_USER} /opt
+
+# STARTING
+# If command starts with an option, prepend syncthing
+# -----------------------------------------------------------------------------
+if [ "${1:0:1}" = '-' ]; then
+	set -- syncthing "$@"
+fi
+
+# If command starts with syncthing, run as container user
+# -----------------------------------------------------------------------------
+if [ "$1" = 'syncthing' ]; then
+	exec gosu ${CONTAINER_USER} "$@"
+fi
+
+# Otherwise start 'normal'
+exec "$@"
