2a9c371a5a65c15284e7eaf589c5b841ff23bd7f
Patrick Kaiser <github@kaiser.xyz>
2017-11-27 10:51:23 +0100

    Remove chmod on data dirs (#17)
    Startup of container could be very slow depending on ammount of data
    in data directories, because of recursive chown operation in
    entrypoint.sh.
    This commit removes the chown of the data directories.

--- images/syncthing/docker-entrypoint.sh
+++ images/syncthing/docker-entrypoint.sh
@@ -16,7 +16,10 @@ usermod -o -u ${LOCAL_UID} -g ${LOCAL_GID} ${CONTAINER_USER}
 
 # Fix Perms
 # -----------------------------------------------------------------------------
-chown -R "${LOCAL_UID}":"${LOCAL_GID}" /home/${CONTAINER_USER} /opt
+echo "[`date`] Start fixing permissions for -- /home/${CONTAINER_USER} /opt/etc /opt/bin -- ..."
+echo "[`date`] Permissions in data dirs /opt/var/* wont be changed! "
+chown -R "${LOCAL_UID}":"${LOCAL_GID}" /home/${CONTAINER_USER} /opt/etc /opt/bin
+chown "${LOCAL_UID}":"${LOCAL_GID}" /opt
 
 # STARTING
 # If command starts with an option, prepend syncthing
